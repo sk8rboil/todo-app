e774efbb665fe9fc409001d9a1fe45234fcd32f4
sk8rboil <sk8rboil008@windowslive.com>
2022-08-18 21:07:11 +0700

    update add_card.dart

--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -2,7 +2,10 @@
 
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:todo_application/app/core/utils/extension.dart';
+import 'package:todo_application/app/data/models/task.dart';
 import 'package:todo_application/app/modules/home/controller.dart';
 import 'package:todo_application/app/widgets/icons.dart';
 
@@ -67,10 +70,36 @@ class AddCard extends StatelessWidget {
                             .toList(),
                       ),
                     ),
-                    ElevatedButton(onPressed: () {}, child: Text('OK')),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.indigoAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          minimumSize: Size(150, 50),
+                        ),
+                        onPressed: () {
+                          if (homeCtrl.formkey.currentState!.validate()) {
+                            int icon =
+                                icons[homeCtrl.chipIndex.value].icon!.codePoint;
+                            String color =
+                                icons[homeCtrl.chipIndex.value].color!.toHex();
+                            var task = Task(
+                                title: homeCtrl.editCtrl.text,
+                                icon: icon,
+                                color: color);
+                            Get.back();
+                            homeCtrl.addTask(task)
+                                ? EasyLoading.showSuccess('create status')
+                                : EasyLoading.showError('duplicate task');
+                          }
+                        },
+                        child: Text('OK')),
                   ],
                 )),
           );
+          homeCtrl.editCtrl.clear();
+          homeCtrl.changeChipIndex(0);
         },
         child: DottedBorder(
             child: Center(
